5418909872224271d923e5ac98fb581d530bd576
Ida Olson <idaolson@gmail.com>
2022-01-15 03:07:45 -0700

    Add current announcements to index method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
+    @current_announcements = Announcement.get_announcements(@user)
+
+    @current_announcements.each do |a|
+      UserAnnouncement.create(user_id: @user.id, announcement_id: a.id)
+    end 
   end
 end
