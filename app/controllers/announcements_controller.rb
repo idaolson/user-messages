b79649ffa0d8dd671009e051719601274adcf7bd
Ida Olson <idaolson@gmail.com>
2022-01-14 20:40:43 -0700

    Create announcements controller

---
+++ app/controllers/announcements_controller.rb
@@ -0,0 +1,26 @@
+class AnnouncementsController < ApplicationController
+  def new
+
+  end 
+
+  def create
+    announcement = Announcement.new(announcement_params)
+    if announcement.save
+  
+      redirect_to new_announcement_path
+      flash[:notice] = "Announcement saved!"
+
+    else
+      flash[:notice] = "Announcement not saved."
+
+      redirect_to new_announcement_path
+    end 
+  end 
+
+  private
+
+  def announcement_params
+    params.permit(:message, :role, :expiration, :active)
+  end 
+
+end 
