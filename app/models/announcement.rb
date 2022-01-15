8236c3148b144efa007c131ccce4181fcfb8fae6
Ida Olson <idaolson@gmail.com>
2022-01-14 20:39:40 -0700

    Create models

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,2 +1,12 @@
 class Announcement < ApplicationRecord
+  has_many :user_announcements, dependent: :destroy
+
+  enum role: [:teacher, :champion, :admin]
+
+  validates :message, :role, :active, :expiration, presence: true
+
+  def populate(announcement)
+    select all from users where user.role == announcement.role 
+    save
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :user_announcements, dependent: :destroy
+
+  enum role: [:teacher, :champion, :admin]
 end
