801f521a75b95d7963571d69fa0a35c9418f7301
Ida Olson <idaolson@gmail.com>
2022-01-14 20:44:22 -0700

    Add before action and helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :require_user
+  helper_method :current_user
+
+  def require_user
+    return if current_user
+
+    redirect_to root_path
+    flash[:notice] = "Sign in to access site"
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
