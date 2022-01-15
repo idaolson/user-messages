ccc13dbe9d35776adde2520c5dbeb2be347de4a5
Ida Olson <idaolson@gmail.com>
2022-01-14 20:42:41 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :users, only: [:new, :create]
+  resources :dashboard, only: [:index]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+  get '/registration', to: 'users#new'
+  post '/registration', to: 'users#create'
+  resources :announcements, only: [:new, :create]
+
 end
+
+
