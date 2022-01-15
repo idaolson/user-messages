64fcac0b68d5e8a6b054a5dd5df79671288d4993
Ida Olson <idaolson@gmail.com>
2022-01-15 11:48:50 -0700

    Make times shown depend on announcement quantity

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,7 +3,7 @@ class Announcement < ApplicationRecord
 
   enum role: [:teacher, :champion, :admin]
 
-  validates :message, :role, :active, :expiration, presence: true
+  validates :message, :role, :active, :expiration, :quantity, presence: true
 
   def self.get_announcements(user)
     announcements = where(role: user.role)
@@ -13,28 +13,12 @@ class Announcement < ApplicationRecord
         SELECT user_announcements.* FROM user_announcements
         WHERE (user_announcements.announcement_id = #{a.id}) AND (user_announcements.user_id = #{user.id})
       ")
-      # ua = select "user_announcements".* from "announcements" where "user_announcements"."announcement_id" = a.id and user_id = user.id
-      if ua.length > 0
+      if ua.length >= a.quantity
         next
       else 
         current_announcements << a
-        # UserAnnoucement.create(user_id: user.id, announcement_id: a.id)
       end 
     end 
     return current_announcements
   end 
 end
-
-
-# SELECT t1.name
-# FROM table1 t1
-# LEFT JOIN table2 t2 ON t2.name = t1.name
-# WHERE t2.name IS NULL
-
-# SELECT "announcments".* FROM "announcments" LEFT OUTER JOIN "user_announcements" ON "user_announcements"."announcement_id" = "announcements"."id"
-# WHERE "user_announcements.id" IS NULL and 
-
-#take all announcements where the user role and announcement role are the same. 
-
-# If you don't have a user_announcement with the user id and the announcement id. 
-# create it. and return the annoucnement. 
