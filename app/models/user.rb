d95f7ebeaa0402ba7a536b38b13844aa8b86b6fe
Ida Olson <idaolson@gmail.com>
2022-01-15 03:11:35 -0700

    Add validations and password user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :user_announcements, dependent: :destroy
 
   enum role: [:teacher, :champion, :admin]
+
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: { case_sensitive: true }
+  
 end
