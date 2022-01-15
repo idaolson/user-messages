09fbe4ec95467dea3602c8f1be0eab56c3c23cfa
Ida Olson <idaolson@gmail.com>
2022-01-15 03:08:36 -0700

    Add additional user columns to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_022614) do
+ActiveRecord::Schema.define(version: 2022_01_15_072737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2022_01_14_022614) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "email"
   end
 
   add_foreign_key "user_announcements", "announcements"
